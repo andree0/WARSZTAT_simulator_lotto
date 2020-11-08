6e68a183784aa0167ac3107cb77220a897fbc36f
andrzej <jonek.andree@gmail.com>
2020-11-09 00:29:51 +0100

    second_try

--- lotto.py
+++ lotto.py
@@ -2,43 +2,29 @@
 from random import shuffle as s
 
 selected_numbers = []
-
-while True:
-    try:
-        selected_numbers.append(int(input("Enter a first number")))
-        while True:
-            try:
-                selected_numbers.append(int(input("Enter a second number")))
-                while True:
-                    try:
-                        selected_numbers.append(int(input("Enter a third number")))
-                        while True:
-                            try:
-                                selected_numbers.append(int(input("Enter a fourth number")))
-                                while True:
-                                    try:
-                                        selected_numbers.append(int(input("Enter a fifth number")))
-                                        while True:
-                                            try:
-                                                selected_numbers.append(int(input("Enter a sixth number")))
-                                                break
-                                            except (ValueError, TypeError):
-                                                print("It's not a number!")
-                                        break
-                                    except (ValueError, TypeError):
-                                        print("It's not a number!")
-                                break
-                            except (ValueError, TypeError):
-                                print("It's not a number!")
-                        break
-                    except (ValueError, TypeError):
-                        print("It's not a number!")
-                break
-            except (ValueError, TypeError):
-                print("It's not a number!")
-        break
-    except (ValueError, TypeError):
-        print("It's not a number!")
+name_numbers = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth']
+for i in range(6):
+    while True:
+        try:
+            selected_numbers.append(int(input(f"Enter a {name_numbers[i]} number")))
+            if 0 < selected_numbers[i] < 50:
+                if selected_numbers[i] in selected_numbers:
+                    break
+                else:
+                    selected_numbers.pop(i)
+                    print("""
+                    You already entered that number!
+                    Choose a different number.
+                    """)
+            else:
+                selected_numbers.pop(i)
+                print("""
+                The number is out of range!
+                You have to select number between 1 and 49.
+                """)
+            # break
+        except ValueError:
+            print("It's not a number!")
 
 print(selected_numbers)
 
